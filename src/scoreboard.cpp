d300cfbd1b12ea5d23b4578911ea4cc3c41caa05
Mikayla Khamnyvong <khamnyvongmik@gmail.com>
2026-07-08 22:50:32 +0000

    Changed sortScores()

--- src/scoreboard.cpp
+++ src/scoreboard.cpp
@@ -8,9 +8,7 @@ int calculateTotal(const int scores[], int size) {
     // Otherwise, return the sum of all scores.
     if(scores == nullptr || !isValidSize(size)) return 0;
     int sum = 0;
-    for(int i = 0; i < size; i++) {
-        sum += scores[i];
-    }
+    for(int i = 0; i < size; i++) sum += scores[i];
     return sum;
 }
 
@@ -26,9 +24,7 @@ int findLowest(const int scores[], int size) {
     // Otherwise, return the smallest score.
     if(scores == nullptr || !isValidSize(size)) return 0;
     int val = scores[0];
-    for(int i = 1; i < size - 1; i++) {
-        if(scores[i] < val) val = scores[i];
-    }
+    for(int i = 1; i < size - 1; i++) if(scores[i] < val) val = scores[i];
     return val;
 }
 
@@ -37,9 +33,7 @@ int findHighest(const int scores[], int size) {
     // Otherwise, return the largest score.
     if(scores == nullptr || !isValidSize(size)) return 0;
     int val = scores[0];
-    for(int i = 1; i < size - 1; i++) {
-        if(scores[i] > val) val = scores[i];
-    }
+    for(int i = 1; i < size - 1; i++) if(scores[i] > val) val = scores[i];
     return val;
 }
 
@@ -47,18 +41,26 @@ int findScore(const int scores[], int size, int target) {
     // Search the array from left to right.
     // Return the index where target is found.
     // Return -1 when target is not found.
-    for(int i = 0; i < size; i++) {
-        if(scores[i] == target) {
-            return i;
-        }
-    }
+    for(int i = 0; i < size; i++) if(scores[i] == target) return i;
     return -1;
 }
 
 void sortScores(int scores[], int size) {
-    // TODO:
     // Sort the array from lowest to highest.
     // A selection sort works well for this lab.
+    for(int start = 0; start < size - 1; start++) {
+        int minIndex = start;
+
+        for(int i = start + 1; i < size; i++) {
+            if(scores[i] < scores[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+        int temp = scores[start];
+        scores[start] = scores[minIndex];
+        scores[minIndex] = temp;
+    }
 }
 
 void printScores(const int scores[], int size) {
